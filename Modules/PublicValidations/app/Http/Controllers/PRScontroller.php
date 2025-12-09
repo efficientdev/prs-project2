b060425f5749ad10aa2a88a320101cf4ef11466c
Monarch <digitalmonarchltd@gmail.com>
2025-12-09 09:17:12 +0100

    tue9dec0916

---
+++ Modules/PublicValidations/app/Http/Controllers/PRScontroller.php
@@ -0,0 +1,48 @@
+<?php
+namespace Modules\PublicValidations\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+ 
+use Modules\PublicValidations\Models\PublicValidation; // Assume this is your model that holds the whole form data 
+use Illuminate\Support\Facades\Session;
+
+    use Illuminate\Support\Facades\Storage;
+
+use App\Models\{City,Lga,SchoolSector,Ward};
+  
+
+class PRScontroller extends Controller
+{
+    public function index(Request $request)
+    {
+        $filters = $request->only(['school_name', 'lga_id', 'ward_id', 'school_level', 'school_category']);
+
+        $lgas  = Ward::select('lga_id', 'lga_name')->distinct()->get();
+        $wards = Ward::select('ward_id', 'ward_name', 'lga_id')->get();
+
+        //->query()
+        $forms = PublicValidation::where('submitted',true)
+            ->when($filters['school_name'] ?? null, function ($q, $v) {
+                $q->where('data->sectionA->school_name', 'LIKE', "%$v%");
+            })
+            ->when($filters['lga_id'] ?? null, function ($q, $v) {
+                $q->where('data->sectionA->lga_id', $v);
+            })
+            ->when($filters['ward_id'] ?? null, function ($q, $v) {
+                $q->where('data->sectionA->ward_id', $v);
+            })
+            ->when($filters['school_level'] ?? null, function ($q, $v) {
+                $q->where('data->sectionA->school_level', $v);
+            })
+            ->when($filters['school_category'] ?? null, function ($q, $v) {
+                $q->where('data->sectionA->school_category', $v);
+            })
+            ->latest()
+            ->paginate(10)
+            ->appends($filters);
+
+        return view("publicvalidations::prs.index", compact('forms', 'filters', 'lgas', 'wards'));
+    }
+ 
+}
--- Modules/Registrations/resources/views/approvals/history2.blade.php
+++ Modules/Registrations/resources/views/approvals/history2.blade.php
@@ -5,13 +5,39 @@
 <p>Status:  {{ ucwords(str_replace('_', ' ', $application->status)) }} </p>
 </div></div>
 
+
      
 @php
     $data=$application->data??[];
     $form=$application??null;
 @endphp
+
+
+@php
+$tabslist=[
+'Proprietor Form',
+'History',
+'CIE Report',
+'Proprietor Form'
+];
+    
+    foreach ($application->approvals as $approval){
+        $tabslist[]=$approval->stage->name; 
+    }
+   
+@endphp
+
+
+
+<x-tabs :labels="$tabslist">
+
+
+    <x-slot name="tab0"> 
+
 @include('registrations::print')
 
+    </x-slot>
+    <x-slot name="tab1"> 
 {{--
 {{$application->registrationPayment}}
 <hr/>
@@ -34,37 +60,51 @@
     @endforelse
 
 
-<ul>
-    @foreach ($application->approvals as $approval)
-        <li class="mt-2 pb-2 border-b">
+    </x-slot>
+
+<!--
+<ul>-->
+    @foreach ($application->approvals as $i=> $approval)
+
+    @php 
+    $ss=$i+2;
+    @endphp
+<x-slot :name="'tab'.$ss">
+        <!--
+        <li class="mt-2 pb-2 border-b">-->
             <strong>{{ $approval->stage->name }}</strong> - {{ $approval->status }}
-@if(!auth()->user()->hasRole('proprietor'))
-             by {{ $approval->user->name ?? 'N/A' }}@endif
+
+            @if(!auth()->user()->hasRole('proprietor'))
+                 by {{ $approval->user->name ?? 'N/A' }}
+            @endif
             <br>
 
-@if($approval->stage->role_name=="CIE")
+            @if($approval->stage->role_name=="CIE")
 
-    @php
-        $report=$application;
-        //
-    @endphp
+                @php
+                    $report=$application;
+                    //
+                @endphp
 
 
-    @if(isset($report->cies_reports))
-        @include('cies::print')
-    @else
-        <div class="text-xl">Not yet available</div>
-    @endif
+                @if(isset($report->cies_reports))
+                    @include('cies::print')
+                @else
+                    <div class="text-xl">Not yet available</div>
+                @endif
 
-@endif
+            @endif
 
 
 
             <div class="flex justify-between"><div>Comment: {{ $approval->comments ?? 'None' }}.</div>
             <div> <i class="far fa-clock mr-1"></i>
                                     Created {{ $approval->created_at->format('M j, Y') }}</div>
-        </li>
+        <!--</li>-->
+    </x-slot>
     @endforeach
-</ul>
+<!--</ul>--> 
+
+</x-tabs>
 
 </div>
