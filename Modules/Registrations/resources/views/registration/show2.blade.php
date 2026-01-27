26985bb173d995b00d3f14dafbdd5814795d3f8e
Monarch <digitalmonarchltd@gmail.com>
2026-01-27 12:01:21 +0100

    tue27jan1201

--- Modules/Registrations/resources/views/registration/show2.blade.php
+++ Modules/Registrations/resources/views/registration/show2.blade.php
@@ -1,7 +1,34 @@
- <x-app-layout>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+<meta content="Monarch" name="author">
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+<link href="favicon.ico" rel="icon" type="image/x-icon"> 
+<link href="{{asset('css/member.min.css')}}" rel="stylesheet" type="text/css">
+<style type="text/css">
+    [x-cloak] { display: none !important; }
+
+</style>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
  @include('registrations::approvals.wholehistory')
 
-
-</x-app-layout>
+</body>
+</html>
